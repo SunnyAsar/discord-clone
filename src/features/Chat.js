80e3f80200c534757eff25ffcf794a1103950851
Sunny Asar <asarsunny@gmail.com>
2020-10-15 10:45:18 +0100

    styled form component

--- src/features/Chat.js
+++ src/features/Chat.js
@@ -2,10 +2,34 @@ import React from 'react'
 import '../styles/chat.css'
 import ChatHeader from './ChatHeader'
 
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+
 function Chat() {
   return (
     <div className='chat'>
       <ChatHeader/>
+
+      <div className="chat__messages">
+
+      </div>
+
+      <div className="chat__input">
+        <AddCircleIcon fontSize='large'/>
+        <form>
+          <input type="text" placeholder='message here...'/>
+          <button className='chat__inputButton' type='submit'> send</button>
+        </form>
+
+        <div className="chat__inputIcons">
+          <CardGiftcardIcon fontSize='large'/>
+          <GifIcon fontSize='large'/>
+          <EmojiEmotionsIcon fontSize='large'/>
+        </div>
+      </div>
     </div>
   )
 }
